f0b9b1e3a8522eee5f0f1fee62a3411ab278eb65
3Teleia <80172777+3Teleia@users.noreply.github.com>
2024-09-28 23:06:32 +0300

    Update triteleia_track_skin.lua

--- change_track_skin/triteleia_track_skin.lua
+++ change_track_skin/triteleia_track_skin.lua
@@ -23,25 +23,28 @@ function change_track_skin(timing, end_timing, skin, easing)
 		elseif tracks[skin] ~= nil then -- If track with desired skin already exists,
 			local start_alpha = track_alpha_channels[skin].valueAt(timing)
 
-			track_alpha_channels[skin].addKey(timing,start_alpha,easing)
-									  .addKey(end_timing,255)
+			track_alpha_channels[skin]
+			.addKey(timing,start_alpha,easing)
+			.addKey(end_timing,255)
 			
 			if current_skin.valueAt(timing) ~= "base" then
 				track_alpha_channels[current_skin.valueAt(timing)].addKey(end_timing,0,"inconst")
 				track_layer_channels[current_skin.valueAt(timing)].addKey(end_timing,top_layer-2) -- Put last used skin track below the new track when the new track is FINISHED fading in
 			end
 			
-			track_layer_channels[skin].addKey(timing,top_layer+1)    -- Put new track on the very top
-									  .addKey(end_timing, top_layer) -- Put new track 1 layer below the topmost layer so that other tracks can go above it later
+			track_layer_channels[skin]
+			.addKey(timing,top_layer+1)    -- Put new track on the very top
+			.addKey(end_timing, top_layer) -- Put new track 1 layer below the topmost layer so that other tracks can go above it later
 
 		else 									  -- If track with desired skin does not exist,
 			tracks[skin] = Scene.track.copy(true) -- Create a copy of the main track
 			tracks[skin].setTrackSprite(skin)     -- Change its skin to the specified skin
 			
-			track_alpha_channels[skin] = Channel.keyframe()							-- Create an exclusive alpha channel for the new track
-												.addKey(-10000,0)					-- Make track start fully transparent
-												.addKey(timing,0,easing)			-- Ensure track stays transparent until it's go time
-												.addKey(end_timing,255,easing)  	-- Fade new track into existence			
+			track_alpha_channels[skin] = 
+			Channel.keyframe()				-- Create an exclusive alpha channel for the new track
+			.addKey(-10000,0)				-- Make track start fully transparent
+			.addKey(timing,0,easing)		-- Ensure track stays transparent until it's go time
+			.addKey(end_timing,255,easing)  -- Fade new track into existence			
 			
 			-- Make the new track use the above channel for its alpha values
 			tracks[skin].colorA = track_alpha_channels[skin]
@@ -61,11 +64,12 @@ function change_track_skin(timing, end_timing, skin, easing)
 				track_layer_channels[current_skin.valueAt(timing)].addKey(end_timing,top_layer-2)	-- Put last used skin track below the new track when the new track is FINISHED fading in
 			end
 			
-			track_layer_channels[skin] = Channel.keyframe().setDefaultEasing('inconst')   -- Create a channel that determines the layer of the new track
-														   .addKey(-10000,top_layer-2)         -- Place it below the main track
-														   .addKey(timing, top_layer+1)   -- Put new track on the top when it starts fading in
-														   .addKey(end_timing, top_layer) -- Put new track 1 layer below the topmost layer
-			tracks[skin].sort = track_layer_channels[skin] -- Make the new track use the above channel for its layering
+			track_layer_channels[skin] = 
+			Channel.keyframe().setDefaultEasing('inconst')  -- Create a channel that determines the layer of the new track
+			.addKey(-10000,top_layer-2)    					-- Place it below the main track
+			.addKey(timing, top_layer+1)   					-- Put new track on the top when it starts fading in
+			.addKey(end_timing, top_layer) 					-- Put new track 1 layer below the topmost layer
+			tracks[skin].sort = track_layer_channels[skin]  -- Make the new track use the above channel for its layering
 			
 			-- The base track does not use textureOffsetY, that cannot be reused here
 			-- track_texture_channels[skin] = Channel.keyframe().addKey(0,0).addKey(1000000,6000) * (current_bpm / base_bpm)
@@ -90,8 +94,9 @@ end)
 function track_skin_display(timing, end_timing, alpha, easing)
     local track_alpha = track_alpha_channels[current_skin.valueAt(timing)]
 
-    track_alpha.addKey(timing, track_alpha.valueAt(timing), easing)
-    		   .addKey(end_timing, alpha)
+    track_alpha
+	.addKey(timing, track_alpha.valueAt(timing), easing)
+    .addKey(end_timing, alpha)
 end
 
 addScenecontrol("trackskindisplay", {"end_timing","alpha","easing"}, function(cmd)
