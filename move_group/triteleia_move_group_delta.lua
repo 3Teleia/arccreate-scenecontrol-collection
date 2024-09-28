21c0ec0fe18956e9255ea6a9a4e311c5537d2124
3Teleia <80172777+3Teleia@users.noreply.github.com>
2024-09-28 22:38:27 +0300

    Update triteleia_move_group_delta.lua

--- move_group/triteleia_move_group_delta.lua
+++ move_group/triteleia_move_group_delta.lua
@@ -2,12 +2,16 @@ local x_channels = {}
 local y_channels = {}
 local z_channels = {}
 
-function move_group_delta(timing, end_timing, tg, x, y, z, easing)
+local x_judge_channels = {}
+local y_judge_channels = {}
+local z_judge_channels = {}
+
+function move_group_visual_delta(timing, end_timing, tg, x, y, z, easing)
 	local movable_tg = Scene.getNoteGroup(tg)
 	
 	if x_channels[tg] == nil then
 		x_channels[tg] = Channel.keyframe()
-		.addKey(0,0)
+		.addKey(-10000,0)
 		.addKey(timing, movable_tg.translationX.valueAt(timing), easing)
 		.addKey(end_timing, x)
 			
@@ -16,7 +20,7 @@ function move_group_delta(timing, end_timing, tg, x, y, z, easing)
 	
 	if y_channels[tg] == nil then
 		y_channels[tg] = Channel.keyframe()
-		.addKey(0,0)
+		.addKey(-10000,0)
 		.addKey(timing, movable_tg.translationY.valueAt(timing), easing)
 		.addKey(end_timing, y)
 			
@@ -25,7 +29,7 @@ function move_group_delta(timing, end_timing, tg, x, y, z, easing)
 	
 	if z_channels[tg] == nil then
 		z_channels[tg] = Channel.keyframe()
-		.addKey(0,0)
+		.addKey(-10000,0)
 		.addKey(timing, movable_tg.translationZ.valueAt(timing), easing)
 		.addKey(end_timing, z)
 			
@@ -52,15 +56,70 @@ function move_group_delta(timing, end_timing, tg, x, y, z, easing)
 	
 end
 
-addScenecontrol("movenotesdelta", {"end_timing","dx","dy","dz","easing"}, function(cmd)
+function move_group_judge_delta(timing, end_timing, tg, x, y, z, easing)
+	local movable_tg = Scene.getNoteGroup(tg)
+	
+	if x_judge_channels[tg] == nil then
+		x_judge_channels[tg] = Channel.keyframe()
+		.addKey(-10000,0)
+		.addKey(timing, movable_tg.judgeOffsetX.valueAt(timing), easing)
+		.addKey(end_timing, x)
+			
+		movable_tg.judgeOffsetX = x_judge_channels[tg]
+	end
+	
+	if y_judge_channels[tg] == nil then
+		y_judge_channels[tg] = Channel.keyframe()
+		.addKey(-10000,0)
+		.addKey(timing, movable_tg.judgeOffsetY.valueAt(timing), easing)
+		.addKey(end_timing, y)
+			
+		movable_tg.judgeOffsetY = y_judge_channels[tg]
+	end
+	
+	if z_judge_channels[tg] == nil then
+		z_judge_channels[tg] = Channel.keyframe()
+		.addKey(-10000,0)
+		.addKey(timing, movable_tg.judgeOffsetZ.valueAt(timing), easing)
+		.addKey(end_timing, z)
+			
+		movable_tg.judgeOffsetZ = z_judge_channels[tg]
+	end
+	
+	if x ~= 0 then
+		x_judge_channels[tg]
+		.addKey(timing, x_judge_channels[tg].valueAt(timing), easing)
+		.addKey(end_timing, x_judge_channels[tg].valueAt(timing) + x)
+	end
+	
+	if y ~= 0 then
+		y_judge_channels[tg]
+		.addKey(timing, y_judge_channels[tg].valueAt(timing), easing)
+		.addKey(end_timing, y_judge_channels[tg].valueAt(timing) + y)
+	end
+	
+	if z ~= 0 then
+		z_judge_channels[tg]
+		.addKey(timing, z_judge_channels[tg].valueAt(timing), easing)
+		.addKey(end_timing, z_judge_channels[tg].valueAt(timing) + z)
+	end
+	
+end
+
+addScenecontrol("movenotesdelta", {"end_timing","dx","dy","dz","easing","movejudge"}, function(cmd)
 	local timing = cmd.timing
 	local end_timing = cmd.args[1]
 	local x = cmd.args[2]
 	local y = cmd.args[3]
 	local z = cmd.args[4]
 	local easing = cmd.args[5]
+	local movejudge = cmd.args[6]
 	
 	local tg = cmd.timingGroup
 	
-	move_group_delta(timing, end_timing, tg, x, y, z, easing)
+	move_group_visual_delta(timing, end_timing, tg, x, y, z, easing)
+
+	if movejudge ~= 0 then
+		move_group_judge_delta(timing, end_timing, tg, x, y, z, easing)
+	end
 end)
